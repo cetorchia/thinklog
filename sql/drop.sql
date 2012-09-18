85387bc230bf9c3c77dff5009d6bb034830af3ce
Carlos Torchia <ctorchia87@gmail.com>
2012-09-17 18:03:00 -0700

    Remove stuff like ARC we do not need anymore

--- sql/drop.sql
+++ sql/drop.sql
@@ -3,3 +3,8 @@
 #
 
 drop table thinkers;
+drop table thoughts;
+drop table keywords;
+drop table common_keywords;
+drop table related_keywords;
+drop table pair_counts;
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,10 +1,46 @@
 #
 # The following SQL is for creating the tables of Thinklog
-# (c) 2010 Carlos E. Torchia
+# (c) 2010,2012 Carlos E. Torchia
 # Licensed under GPLv2
 #
 
 create table if not exists thinkers (
 	thinker_id varchar(64) primary key,
-	password varchar(40)
+	password varchar(40),
+	about varchar(512),
+	name varchar(64)
 );
+
+create table if not exists thoughts (
+	thought_id integer primary key auto_increment,
+	content varchar(2048),
+	thinker_id varchar(64),
+	date timestamp default current_timestamp,
+	private tinyint
+);
+
+create table if not exists keywords (
+	keyword_id integer primary key auto_increment,
+	keyword varchar(128)
+);
+
+create table if not exists common_keywords (
+	keyword_id integer primary key
+);
+
+create table if not exists mentions (
+	thought_id integer,
+	keyword_id integer
+);
+
+create table if not exists related_keywords (
+	keyword1 integer,
+	keyword2 integer
+);
+
+create table if not exists pair_counts (
+	keyword1 integer,
+	keyword2 integer,
+	count integer
+);
+
