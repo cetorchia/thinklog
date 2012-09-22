9d282372e3113181a6bd47c0df80c051ff4ac681
Carlos Torchia <ctorchia87@gmail.com>
2012-09-22 06:01:43 -0700

    Make archive of Thinklog and put it on webhome

--- deploy.uvic.sh
+++ deploy.uvic.sh
@@ -4,6 +4,14 @@
 # (c) Carlos E. Torchia under GPL2
 #
 
+# Deploy PHP app on UVic web server
 ssh uvic -C 'rm -rf www/thinklog ; mkdir www/thinklog'
 scp -r * uvic:www/thinklog/
 ssh uvic -C 'mv www/thinklog/def.php.uvic www/thinklog/def.php'
+
+# Create archive and put in on CSc web server
+cd ..
+zip -r thinklog-1.0.zip thinklog/*
+zip -d thinklog-1.0.zip thinklog/def.php.uvic
+scp thinklog-1.0.zip webhome:public_html/
+rm thinklog-1.0.zip
