e63872cd1c7484d9dfeb557aae0fa316d833e558
Carlos Torchia <ctorchia87@gmail.com>
2012-12-28 01:50:00 -0800

    Make keyword history day queries into one query using UNIONs

--- classes/KeywordHistoryService.php
+++ classes/KeywordHistoryService.php
@@ -14,6 +14,7 @@ class KeywordHistoryService
 	//
 	function getKeywordHistory($thinkerId, $query)
 	{
+		// Generate the query statement
 		if ($query) {
 			$words = $this->keywordService->getWords($query);
 			$wordlist = $this->keywordService->getKeywordsSQL($words);
@@ -22,30 +23,36 @@ class KeywordHistoryService
 		}
 		$thinkerSQL = $thinkerId ? "t.thinker_id = '$thinkerId'" : "TRUE";
 
-		$keywordHistory = array();
 		for ($day = 0; $day < 30; $day++) {
-			// Get the keyword history rows
-			$result = $this->_queryDay($thinkerSQL, $wordlist, $day);
-			while ($row = mysql_fetch_array($result)) {
-				$keyword = $row["keyword"];
-				$count = (int)$row["cnt"];
-				if (!isset($keywordHistory[$keyword])) {
-					$keywordHistory[$keyword] = array_fill(0, 30, 0);
-				}
-				$keywordHistory[$keyword][$day] = $count;
+			$query .= "(" . $this->getDayQuery($thinkerSQL, $wordlist, $day) . ")";
+			if ($day < 29) {
+				$query .= " UNION ";
 			}
+		}
+
+		// Retrieve the keyword day counts
+		$keywordHistory = array();
+		$result = mysql_query($query);
+		while ($row = mysql_fetch_array($result)) {
+			$keyword = $row["keyword"];
+			$count = (int)$row["cnt"];
+			$day = (int)$row["day"];
+			if (!isset($keywordHistory[$keyword])) {
+				$keywordHistory[$keyword] = array_fill(0, 30, 0);
+			}
+			$keywordHistory[$keyword][$day] = $count;
 		}
 
 		return $keywordHistory;
 	}
 
-	// Returns the MySQL query result for given thinker, query, and day
-	protected function _queryDay($thinkerSQL, $wordlist, $day) {
+	// Returns the MySQL SELECT clause for given thinker, query, and day
+	protected function getDayQuery($thinkerSQL, $wordlist, $day) {
 		$daySQL = "-FLOOR(UNIX_TIMESTAMP(current_timestamp)/86400)+FLOOR(UNIX_TIMESTAMP(date)/86400)+29";
 
 		// Query for keywords related to this thinker and query and count how many for each day
 		if($wordlist) {
-			$query = "SELECT k2.keyword as keyword, COUNT(t.thought_id) as cnt " .
+			$query = "SELECT k2.keyword as keyword, COUNT(t.thought_id) as cnt, $day as day " .
 			         " FROM keywords k1, keywords k2, mentions m1, mentions m2, thoughts t " .
 			         " WHERE k1.keyword IN ($wordlist) " .
 			         "   AND k1.keyword_id = m1.keyword_id " .
@@ -57,7 +64,7 @@ class KeywordHistoryService
 			         " GROUP BY k2.keyword_id ".
 			         " ORDER BY cnt DESC LIMIT 10 ";
 		} else {
-			$query = "SELECT k.keyword as keyword, COUNT(t.thought_id) as cnt " .
+			$query = "SELECT k.keyword as keyword, COUNT(t.thought_id) as cnt, $day as day " .
 			         " FROM keywords k, mentions m, thoughts t " .
 			         " WHERE k.keyword_id = m.keyword_id " .
 			       	 "   AND t.thought_id = m.thought_id " .
@@ -66,6 +73,6 @@ class KeywordHistoryService
 			         " GROUP BY k.keyword_id ".
 			         " ORDER BY cnt DESC LIMIT 10 ";
 		}
-		return mysql_query($query);
+		return $query;
 	}
 }
